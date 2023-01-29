ce0f7b80d0c091d8b6e58d4df414a027131f8e90
capareyyupcan <capareyyupcan@gmail.com>
2023-01-30 00:09:18 +0300

    Next13Film

---
+++ app/layout.jsx
@@ -0,0 +1,31 @@
+import "./global.css";
+import { Montserrat } from "@next/font/google";
+
+const montserrat = Montserrat({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr" className="bg-slate-900 text-white">
+      <head />
+
+      <body className={`${montserrat.className} mx-32 my-12`} >
+        {/* <nav>
+          <h1>Logo</h1>
+          <ul>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Sign up</a>
+            </li>
+          </ul>
+        </nav> */}
+        {children}
+      </body>
+    </html>
+  );
+}
